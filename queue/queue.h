d6ea83122703d1fb98069e263d76fc10eff5d8b5
Mattia Shux <82611659+Xhst@users.noreply.github.com>
2024-01-20 11:51:50 +0100

    queue header

---
+++ queue/queue.h
@@ -0,0 +1,45 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+struct queue_node {
+    void* value;
+    struct queue_node* next;
+    struct queue_node* prev;
+};
+
+typedef struct queue {
+    struct queue_node* head;
+    struct queue_node* tail;
+} Queue;
+
+/**
+ * Returns reference to an empty queue.
+ */
+struct queue* new_queue();
+
+/**
+ * Add an element to the queue.
+ */
+void enqueue(struct queue* queue, void* object);
+
+/**
+ * Returns and remove the oldest element in the queue.
+ */
+void* dequeue(struct queue* queue);
+
+/**
+ * Returns the oldest element in the queue.
+ */
+void* peek(struct queue* queue);
+
+/**
+ * Returns the number of elements in the queue.
+ */
+int size(struct queue* queue);
+
+/**
+ * Returns 1 if the queue is empty, 0 otherwise.
+ */
+int is_empty(struct queue* queue);
+
+#endif

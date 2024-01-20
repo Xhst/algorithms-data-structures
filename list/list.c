d89a013248f863f3fcffbc5481deea12d20f7ba9
Mattia Shux <82611659+Xhst@users.noreply.github.com>
2024-01-20 17:35:53 +0100

    list implementation

---
+++ list/list.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+#include "list.h"
+
+List* new_list() {
+    List* list = malloc(sizeof(List));
+
+    list->first = NULL;
+    list->size = 0;
+
+    return list;
+}
+
+void* get(List* list, int position) {
+    list_node* node = list->first;
+    int pos = 0;
+
+    if (position > list->size - 1) return NULL;
+
+    while (pos < position) {
+        node = node->next;
+        pos++;
+    }
+
+    return node;
+}
+
+void add(List* list, void* object) {
+    list_node* node = malloc(sizeof(list_node));
+    node->value = object;
+
+    if (list->first != NULL) {
+        node->next = list->first;
+    }
+
+    list->first = node;
+    list->size++;
+}
+
+void remove_at(List* list, int position) {
+    list_node* prev;
+    list_node* node = list->first;
+    int pos = 0;
+
+    if (position > list->size - 1) return;
+
+    while (pos < position) {
+        prev = node;
+        node = node->next;
+        pos++;
+    }
+
+    prev->next = node->next;
+    list->size--;
+
+    free(node);
+}
+
+void remove_first(List* list, void* value) {
+    list_node* prev;
+    list_node* node = list->first;
+
+    while (node != NULL && node->value != value) {
+        prev = node;
+        node = node->next;
+    }
+
+    if (node->value != value) return;
+
+    prev->next = node->next;
+    list->size--;
+
+    free(node);
+}
+
+int size(List* list) {
+    if (list == NULL) return 0;
+
+    return list->size;
+}
+
+int is_empty(List* list) {
+    return list == NULL || list->size == 0;
+}
